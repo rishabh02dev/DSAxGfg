01d9ffc91efc8567c625702d7401108f95942955
Rishabh Shukla <kumkumshukla631@gmail.com>
2024-05-30 23:21:47 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Armstrong Numbers/armstrong-numbers.java
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.util.*;
+import java.io.*;
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int n = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+
+            System.out.println(ob.armstrongNumber(n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution {
+    static String armstrongNumber(int n){
+        // code here
+        int sum = 0;
+        int lastdigit = 0;
+        int original = n;
+        while(n>0){
+            lastdigit = n%10;
+            sum = sum + lastdigit*lastdigit*lastdigit;
+            n = n/10;
+        }
+        
+        if(sum==original){
+            return "Yes";
+        }
+        else{
+            return "No";
+        }
+    }
+}
