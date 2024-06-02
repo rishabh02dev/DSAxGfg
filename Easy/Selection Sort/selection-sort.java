1e7232571fa0adab520416275fad667ef319e38d
Rishabh Shukla <kumkumshukla631@gmail.com>
2024-06-02 12:59:54 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Selection Sort/selection-sort.java
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class GFG
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t>0)
+		{
+			int n = sc.nextInt();
+			int arr[] = new int[n]; 
+			for(int i=0;i<n;i++)
+			{
+				arr[i] = sc.nextInt();
+			}
+		
+			Solution obj = new Solution();
+			obj.selectionSort(arr, n);
+			
+			for(int i=0;i<n;i++)
+		    	System.out.print(arr[i]+" ");
+		    System.out.println();
+			t--;
+		}
+		
+	}
+}
+
+// } Driver Code Ends
+
+
+class Solution
+{
+	int  select(int arr[], int i)
+	{
+        int minIndex = i;
+        for (int j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        return minIndex;
+        
+	}
+	
+	void selectionSort(int arr[], int n)
+	{
+	    //code here
+	    for (int i = 0; i < n - 1; i++) {
+            // Find the index of the minimum element in the remaining unsorted part
+            int minIndex = select(arr, i);
+
+            // Swap the found minimum element with the first element
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+
+	    
+	}
+}
+
