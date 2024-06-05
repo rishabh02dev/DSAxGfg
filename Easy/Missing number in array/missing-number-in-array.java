80c7c93b8a9bbe121265f859f3813e878ef23a4d
Rishabh Shukla <kumkumshukla631@gmail.com>
2024-06-05 10:30:17 +0530

    Updated Solution - GfG to GitHub

--- Easy/Missing number in array/missing-number-in-array.java
+++ Easy/Missing number in array/missing-number-in-array.java
@@ -27,19 +27,55 @@ class GFG {
 
 // User function Template for Java
 
-
 class Solution {
-    public int missingNumber(int[] array, int n) {
-        // Calculate the expected sum of the first n natural numbers
-        int expectedSum = n * (n + 1) / 2;
-
-        // Calculate the actual sum of elements in the array
-        int actualSum = 0;
-        for (int num : array) {
-            actualSum += num;
+    int missingNumber(int arr[], int n) {
+        // Your Code Here
+        // THis is the better approach
+        
+        int xor1 = 0;
+        for(int i=1;i<=n;i++){
+            xor1 = xor1^i;
         }
-
-        // Find the missing number by subtracting actual sum from expected sum
-        return expectedSum - actualSum;
+        
+        int xor2 = 0;
+        for(int i=0;i<n-1;i++){
+            xor2 = xor2^arr[i];
+        }
+        
+        return xor1^xor2;
+       
+        
     }
 }
+
+
+
+//This is the brute force approach using normal flag
+// for(int i=1;i<=n;i++){
+//             int flag  = 0;
+//             for(int j=0; j<n-1;j++){
+//                 if(arr[j]==i){
+//                     flag = 1;
+//                     break;
+//                 }
+//             }
+//             if(flag==0){
+//                 return i;
+//             }
+//         }
+        
+//         return 0;
+
+ // int hash[] = new int[n+1];
+        
+        // for(int i=0;i<n-1;i++){
+        //     hash[arr[i]] = 1;
+        // }
+        
+        // for(int i=1; i<=n;i++){
+        //     if(hash[i]==0){
+        //         return i;
+        //     }
+        // }
+        
+        // return -1;
