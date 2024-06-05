be800a0ad1cb284d139417cc17e953b87595cd52
Rishabh Shukla <kumkumshukla631@gmail.com>
2024-06-05 09:43:00 +0530

    Updated Solution - GfG to GitHub

--- Medium/Union of Two Sorted Arrays/union-of-two-sorted-arrays.java
+++ Medium/Union of Two Sorted Arrays/union-of-two-sorted-arrays.java
@@ -57,24 +57,35 @@ class Solution
     public static ArrayList<Integer> findUnion(int arr1[], int arr2[], int n, int m)
     {
         // add your code here
-        HashSet<Integer> st = new HashSet<>();
+        ArrayList<Integer> al = new ArrayList<>();
         
-        //Ab hm log arr1 aur arr2 se uthakar set mai dal dengee
-        for(int i=0;i<n;i++){
-            st.add(arr1[i]);
+        int i=0,j=0;
+        while(i<n && j<m){
+            if(arr1[i]<=arr2[j]){
+                if(al.size()==0 || al.get(al.size()-1)!=arr1[i]){
+                    al.add(arr1[i]);
+                }
+                i++;
+            }else{
+                if(al.size()==0 || al.get(al.size()-1)!=arr2[j]){
+                    al.add(arr2[j]);}
+                j++;
+            }
         }
-        for(int i=0; i<m;i++){
-            st.add(arr2[i]);
+        
+        while(i<n){
+            if(al.get(al.size()-1)!=arr1[i]){
+                al.add(arr1[i]);
+            }
+            i++;
+        }
+        while(j<m){
+            if(al.get(al.size()-1)!=arr2[j]){
+                al.add(arr2[j]);
+            }
+            j++;
         }
         
-        ArrayList<Integer> list1 = new ArrayList<>();
-        for(int it: st){
-            list1.add(it);
-        }
-        
-        Collections.sort(list1);
-        
-        
-        return list1;
+        return al;
     }
 }
