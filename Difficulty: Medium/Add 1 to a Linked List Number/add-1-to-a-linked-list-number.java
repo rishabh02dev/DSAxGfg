34bc803b35af1128619f93f394f02adfa8dce5fe
Rishabh Shukla <kumkumshukla631@gmail.com>
2024-07-08 14:58:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Add 1 to a Linked List Number/add-1-to-a-linked-list-number.java
@@ -0,0 +1,104 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+class Node
+{
+    int data;
+    Node next;
+    
+    Node(int x)
+    {
+        data = x;
+        next = null;
+    }
+}
+class GfG
+{
+    public static void printList(Node node) 
+    { 
+        while (node != null)
+        { 
+            System.out.print(node.data);
+            node = node.next; 
+        }  
+        System.out.println();
+    } 
+    public static void main(String args[])throws IOException
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    String s = sc.next();
+                    Node head = new Node( s.charAt(0) - '0' );
+                    Node tail = head;
+                    for(int i=1; i<s.length(); i++)
+                    {
+                        tail.next = new Node( s.charAt(i) - '0' );
+                        tail = tail.next;
+                    }
+                    Solution obj = new Solution();
+                    head = obj.addOne(head);
+                    printList(head); 
+                }
+        }
+}
+// } Driver Code Ends
+
+
+/*
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+} 
+*/
+
+class Solution
+{
+    
+    
+    static Node reverse(Node head) {
+        Node prev = null;
+        Node current = head;
+        Node next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+    public static Node addOne(Node head) 
+    { 
+        //code here.
+        head = reverse(head);
+
+        Node current = head;
+        int carry = 1;
+
+        // Traverse the reversed list and add one
+        while (current != null) {
+            int sum = current.data + carry;
+            carry = sum / 10;
+            current.data = sum % 10;
+            if (carry == 0) {
+                break;
+            }
+            if (current.next == null && carry != 0) {
+                current.next = new Node(carry);
+                break;
+            }
+            current = current.next;
+        }
+
+        // Reverse the list again to restore the original order
+        head = reverse(head);
+        return head;
+    }
+}
