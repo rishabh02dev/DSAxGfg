166662935b484c0a188883a3e7c5b0ff7ddd89b5
Rishabh Shukla <117344508+rishabh02dev@users.noreply.github.com>
2024-07-12 22:26:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find XOR of numbers from L to R./find-xor-of-numbers-from-l-to-r..java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t;
+        t = sc.nextInt();
+        while (t-- > 0) {
+
+            int l;
+            l = sc.nextInt();
+
+            int r;
+            r = sc.nextInt();
+
+            Solution obj = new Solution();
+            int res = obj.findXOR(l, r);
+
+            System.out.println(res);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    
+    static int findans(int n){
+        
+        if(n%4==1) return 1;
+        else if(n%4==2) return n+1;
+        else if(n%4==3) return 0;
+        else return n;
+    }
+    public static int findXOR(int l, int r) {
+        int val1 = findans(l-1);
+        int val2 = findans(r);
+        
+        return val1^val2;
+    }
+}
