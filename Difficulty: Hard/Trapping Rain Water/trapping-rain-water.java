1fca683decce9e2306d94b251ccc6179da21abca
Rishabh Shukla <117344508+rishabh02dev@users.noreply.github.com>
2024-07-15 02:09:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Trapping Rain Water/trapping-rain-water.java
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+
+class Array {
+
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		while(t-->0){
+		  
+		    //size of array
+		    int n = Integer.parseInt(br.readLine().trim());
+		    int arr[] = new int[n];
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    
+		    //adding elements to the array
+		    for(int i=0; i<n; i++){
+		        arr[i] = Integer.parseInt(inputLine[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    
+		    //calling trappingWater() function
+		    System.out.println(obj.trappingWater(arr, n));
+		}
+	}
+}
+
+
+// } Driver Code Ends
+
+
+class Solution{
+    
+    // arr: input array
+    // n: size of array
+    // Function to find the trapped water between the blocks.
+    static long trappingWater(int arr[], int n) { 
+        // Your code here
+        
+        long[] lmax = new long[n];
+        long[] rmax = new long[n];
+        
+        lmax[0] = arr[0];
+        rmax[n-1] = arr[n-1];
+        
+        for(int i=1; i<n;i++){
+            lmax[i] = Math.max(lmax[i-1],arr[i]);
+        }
+        
+        for(int i = n-2;i>=0;i--){
+            rmax[i] = Math.max(rmax[i+1],arr[i]);
+        }
+        
+        long[] water = new long[n];
+        long sum = 0;
+        for(int i=0; i<n;i++){
+            water[i] = Math.min(lmax[i],rmax[i]) - arr[i];
+            sum = sum+water[i];
+        }
+        
+        return sum;
+    
+    } 
+}
+
+
