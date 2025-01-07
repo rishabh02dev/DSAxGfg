737a6b9a2d7f0302c1823832488cb031d04030da
Rishabh Shukla <kumkumshukla631@gmail.com>
2025-01-07 09:55:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Pair with given sum in a sorted array/pair-with-given-sum-in-a-sorted-array.java
@@ -0,0 +1,115 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+// User function Template for Java
+
+class Solution {
+
+    int countPairs(int arr[], int target) {
+        // Complete the function
+        
+        
+        
+        
+        
+        
+        
+         Map<Integer, Integer> freqMap = new HashMap<>();
+        int count = 0;
+
+        for (int num : arr) {
+            int complement = target - num;
+
+            // Check if the complement exists in the map
+            if (freqMap.containsKey(complement)) {
+                count += freqMap.get(complement);
+            }
+
+            // Update the frequency of the current number
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+
+        return count;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
+
+
+//{ Driver Code Starts.
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+            String[] inputLine = br.readLine().trim().split(" ");
+            int[] arr = new int[inputLine.length];
+            for (int i = 0; i < inputLine.length; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+            inputLine = br.readLine().trim().split(" ");
+            int target = Integer.parseInt(inputLine[0]);
+
+            Solution obj = new Solution();
+            int res = obj.countPairs(arr, target);
+            System.out.println(res);
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
